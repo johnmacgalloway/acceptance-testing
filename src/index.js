d06f26437267f49ac38a8982be17092eeaa887c5
johnmacgalloway <johnmacg@andersonlawrence.co.uk>
2016-09-23 15:20:39 +0100

    minor edit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { fetch } from './ducks/forecast';
 import Weather from './components/weather';
 import createStore from './store';
 
-const CITY = 'Aberdeen';
+const ABERDEEN = 'Aberdeen';
 
 const store = createStore()
     , root = document.getElementById('root');
@@ -17,4 +17,4 @@ ReactDOM.render(
   </Provider>
 , root);
 
-store.dispatch(fetch(CITY));
+store.dispatch(fetch(ABERDEEN));
